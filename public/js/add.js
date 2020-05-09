2c9b761b7622ee8de4a3da8f2676046d8116abdd
Roy Reyna <royreyna7@yahoo.com>
2020-05-09 13:47:44 -0500

    updating add.js methods

--- public/js/add.js
+++ public/js/add.js
@@ -1,28 +1,32 @@
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+// Get references to page elements 
+// Might need to change ID's based on front end
+
+var $name = $("#name");
+var $hireDate = $("#hireDate");
+var $birthday = $("#birthday");
+var $department = $("#department");
+var $pay = $("#pay");
+var $comments = $("#comments");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  addEmployee: function(example) {
+  addEmployee: function (record) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
       },
       type: "POST",
       url: "api/employees",
-      data: JSON.stringify(example)
+      data: JSON.stringify(record)
     });
   },
-  getEmployees: function() {
+  getEmployees: function () {
     return $.ajax({
       url: "api/employees",
       type: "GET"
     });
   },
-  deleteEmployee: function(id) {
+  deleteEmployee: function (id) {
     return $.ajax({
       url: "api/employees/" + id,
       type: "DELETE"
@@ -30,18 +34,18 @@ var API = {
   }
 };
 
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshEmployees = function() {
-  API.getEmployees().then(function(data) {
-    var $examples = data.map(function(example) {
+// refreshEmployees gets new employees from the db and repopulates the list
+var refreshEmployees = function () {
+  API.getEmployees().then(function (data) {
+    var $employee = data.map(function (employee) {
       var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
+        .text(employee.text)
+        .attr("href", "/employees/" + employee.id);
 
       var $li = $("<li>")
         .attr({
           class: "list-group-item",
-          "data-id": example.id
+          "data-id": employee.id
         })
         .append($a);
 
@@ -55,41 +59,57 @@ var refreshEmployees = function() {
     });
 
     $exampleList.empty();
-    $exampleList.append($examples);
+    $exampleList.append($employee);
   });
 };
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+var handleFormSubmit = function (event) {
   event.preventDefault();
 
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
+  var employee = {
+    name: $name.val().trim(),
+    hire_date: $hireDate.val().trim(),
+    birthday: $birthday.val().trim(),
+    department: $department.val().trim(),
+    pay: parseFloat($pay.val().trim()),
+    comments: $comments.val().trim()
   };
+  /*
+  
+    name: DataTypes.STRING,
+    hire_date: DataTypes.DATE,
+    birthday: DataTypes.DATE,
+    department: DataTypes.STRING,
+    pay: DataTypes.DECIMAL(10,2),
+    comments: DataTypes.TEXT*/
 
-  if (!(example.text && example.description)) {
+  if (!(employee.name && employee.hire_date)) {
     alert("You must enter an example text and description!");
     return;
   }
 
-  API.saveExample(example).then(function() {
+  API.addExample(employee).then(function () {
     refreshExamples();
   });
 
-  $exampleText.val("");
-  $exampleDescription.val("");
+  $("#name").val("");
+  $("#hireDate").val("");
+  $("#birthday").val("");
+  $("#department").val("");
+  $("#pay").val("");
+  $("#comments").val("");
 };
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
+var handleDeleteBtnClick = function () {
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
+  API.deleteExample(idToDelete).then(function () {
     refreshExamples();
   });
 };
