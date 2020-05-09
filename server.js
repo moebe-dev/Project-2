ea5d85ce472419e4e4334e026609ff010fc303a7
Roy Reyna <royreyna7@yahoo.com>
2020-05-09 11:59:01 -0500

    changes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.set("view engine", "handlebars");
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-require("./routes/loginRoutes")(app);
+//require("./routes/loginRoutes")(app);
 
 var syncOptions = { force: false };
 
